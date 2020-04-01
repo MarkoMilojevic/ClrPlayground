4d951af6ab1fb4ec0c40babb35ffff0256f9995b
Marko Milojevic <milojevicmarko92@gmail.com>
2020-04-01 19:34:23 +0200

    md bin\Debug

--- simple-lib.bat
+++ simple-lib.bat
@@ -6,6 +6,8 @@ setlocal
 
 set ProjectDir=.\SimpleLib
 
+if not exist %ProjectDir%\bin\Debug md %ProjectDir%\bin\Debug
+
 if not "%1" == "" set OutDir=%1
 if "%1" == "" set OutDir=%ProjectDir%\bin\Debug
 
